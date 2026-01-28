387356c8f92d66c7b1e24b64020ca0e1d06f3515
Chunyang Li <2326074665@qq.com>
2026-01-28 20:46:46 +0800

    add assets

---
+++ bg.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+img = Image.open('/data1/lcy/projects/Space-Time-Pilot/assets/logo.png')
+if img.mode == 'RGBA':
+    background = Image.new('RGBA', img.size, (255, 255, 255, 255))
+    background.paste(img, mask=img.split()[3])
+    background.convert('RGB').save('/data1/lcy/projects/Space-Time-Pilot/assets/logo_white.png')
+else:
+    img.save('/data1/lcy/projects/Space-Time-Pilot/assets/logo_white.png')
+print('Done')
